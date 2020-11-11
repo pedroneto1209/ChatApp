14949567a6f007f37995f5b713b9d3b9a752b86f
Pedro Neto <pedroneto12092000@hotmail.com>
2020-11-11 12:47:26 -0300

    pages flow working

--- lib/main.dart
+++ lib/main.dart
@@ -23,14 +23,17 @@ class Screen extends StatefulWidget {
 class _ScreenState extends State<Screen> with AfterLayoutMixin<Screen> {
   Future checkFirstSeen() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool _seen = (prefs.getBool('seen') ?? false);
+    //prefs.clear();
+    String _seen = '';
+    _seen = prefs.getString('seen');
 
-    if (_seen) {
-      prefs.clear();
+    if (_seen == 'peu') {
       Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
+          .pushReplacement(MaterialPageRoute(builder: (context) => Peu()));
+    } else if (_seen == 'let') {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => Let()));
     } else {
-      await prefs.setBool('seen', true);
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => IntroScreen()));
     }
@@ -55,20 +58,57 @@ class _ScreenState extends State<Screen> with AfterLayoutMixin<Screen> {
   }
 }
 
-class Home extends StatelessWidget {
+class Let extends StatefulWidget {
+  @override
+  _LetState createState() => _LetState();
+}
+
+class _LetState extends State<Let> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/initback.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: null /* add child content here */,
-      ),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(color: Colors.red),
+        FloatingActionButton(
+          onPressed: () {
+            clear();
+          },
+        )
+      ],
     );
+  }
+
+  clear() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+}
+
+class Peu extends StatefulWidget {
+  @override
+  _PeuState createState() => _PeuState();
+}
+
+class _PeuState extends State<Peu> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(color: Colors.blue),
+        FloatingActionButton(
+          onPressed: () {
+            clear();
+          },
+        )
+      ],
+    );
+  }
+
+  clear() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
   }
 }
 
@@ -102,15 +142,92 @@ class IntroScreen extends StatelessWidget {
                 ),
                 Container(height: height * 0.05),
                 GestureDetector(
-                  child: Container(color: Colors.red, height: height * 0.2),
+                  onTap: () {
+                    route('let', context);
+                  },
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: width * 0.8,
+                        height: height * 0.2,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(width * 0.05)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        'lequinha',
+                        style: TextStyle(
+                            fontFamily: 'Let', fontSize: width * 0.15),
+                      ),
+                    ],
+                  ),
                 ),
                 Container(height: height * 0.05),
                 GestureDetector(
-                  child: Container(color: Colors.red, height: height * 0.2),
+                  onTap: () {
+                    route('peu', context);
+                  },
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: width * 0.8,
+                        height: height * 0.2,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(width * 0.05)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        'PEPEOU',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Peu',
+                            fontSize: width * 0.15),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           )),
     );
+  }
+
+  Future<void> route(String rota, BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('seen', rota);
+    Widget destiny;
+
+    if (rota == 'peu') {
+      destiny = Peu();
+    } else {
+      destiny = Let();
+    }
+
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => destiny));
   }
 }
